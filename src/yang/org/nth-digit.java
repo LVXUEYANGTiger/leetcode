70e9b5456c831c683f95140fd04a6a16f3465a45
LvXueYangTiger <1191432275@qq.com>
2022-08-31 17:47:35 +0800

    完成第N位数字,思想是模拟,注意点:位数的处理

---
+++ src/yang/org/nth-digit.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int findNthDigit(int n) {
+        if(n <= 9){
+            return n;
+        }
+       // 1.求出这个数是几位数
+        int index = 1;
+        long sum = 0;
+        for (;index <= 9;index++) {
+            long count =  (long) index * ((long) Math.pow(10, index) - (long) Math.pow(10, index - 1));
+            if(count + sum >= n){
+                break;
+            }
+            sum += count;
+        }
+       // 2.找出这个数
+        long minNum = (long) Math.pow(10,index - 1);
+        long add = (n - sum - 1) / index;
+        long target = minNum + add;
+       // 3.求模
+        int mod = (int) ((n - sum - 1) % index);
+
+        return String.valueOf(target).charAt(mod) - '0';
+
+    }
+}
+// @solution-sync:end
+
+class Main {
+
+    public static void main(String[] args) {
+        int n = 11;
+
+        int result = new Solution().findNthDigit(n);
+        System.out.println(result);
+    }
+
+}
